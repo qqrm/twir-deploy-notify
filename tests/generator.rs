4949af752d535e822ad5a8b986a8da8c22121ec0
Alexey <4681325+qqrm@users.noreply.github.com>
2025-07-05 16:05:10 +0300

    Handle escaping when splitting posts

--- tests/generator.rs
+++ tests/generator.rs
@@ -10,6 +10,7 @@ mod validator;
 
 use generator::{TELEGRAM_LIMIT, split_posts};
 use proptest::prelude::*;
+use validator::validate_telegram_markdown;
 
 fn arb_long_line() -> impl Strategy<Value = String> {
     let regex = format!(
@@ -17,6 +18,13 @@ fn arb_long_line() -> impl Strategy<Value = String> {
         TELEGRAM_LIMIT * 2,
         TELEGRAM_LIMIT * 3
     );
+    proptest::string::string_regex(&regex).unwrap()
+}
+
+fn arb_escaped_text() -> impl Strategy<Value = String> {
+    let lower = TELEGRAM_LIMIT - 5;
+    let upper = TELEGRAM_LIMIT + 5;
+    let regex = format!(r"(?:[A-Za-z]|\\\\[-!#\\.]){{{lower},{upper}}}");
     proptest::string::string_regex(&regex).unwrap()
 }
 
@@ -29,6 +37,18 @@ proptest! {
         prop_assert!(!posts.is_empty());
         for p in posts {
             prop_assert!(p.len() <= TELEGRAM_LIMIT);
+        }
+    }
+}
+
+proptest! {
+    #![proptest_config(ProptestConfig::with_cases(16))]
+    #[test]
+    fn escaped_chunks_are_valid(line in arb_escaped_text()) {
+        let posts = split_posts(&line, TELEGRAM_LIMIT);
+        prop_assert!(!posts.is_empty());
+        for p in posts {
+            prop_assert!(validate_telegram_markdown(&p).is_ok());
         }
     }
 }
