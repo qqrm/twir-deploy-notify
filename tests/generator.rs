eef38e42730e92e7f89f1fd535a920fbbdcc4d5b
Alexey <4681325+qqrm@users.noreply.github.com>
2025-07-05 17:21:00 +0300

    test: ensure dash escape at split boundary (#145)

--- tests/generator.rs
+++ tests/generator.rs
@@ -12,6 +12,17 @@ use generator::{TELEGRAM_LIMIT, split_posts};
 use proptest::prelude::*;
 use validator::validate_telegram_markdown;
 
+fn arb_dash_boundary() -> impl Strategy<Value = String> {
+    let prefix_re = format!(r"[A-Za-z0-9]{{{}}}", TELEGRAM_LIMIT - 1);
+    proptest::string::string_regex(&prefix_re)
+        .unwrap()
+        .prop_flat_map(|pre| {
+            proptest::string::string_regex("[A-Za-z0-9]{0,10}")
+                .unwrap()
+                .prop_map(move |post| format!("{pre}\\-{post}"))
+        })
+}
+
 fn arb_long_line() -> impl Strategy<Value = String> {
     let regex = format!(
         r"[A-Za-z0-9\\*_]{{{},{}}}",
@@ -53,6 +64,15 @@ proptest! {
     }
 }
 
+proptest! {
+    #![proptest_config(ProptestConfig::with_cases(16))]
+    #[test]
+    fn dash_boundary_preserves_escape(text in arb_dash_boundary()) {
+        let posts = split_posts(&text, TELEGRAM_LIMIT);
+        prop_assert!(!posts.is_empty());
+    }
+}
+
 fn arb_dash_boundary() -> impl Strategy<Value = String> {
     let prefix_regex = format!(r"[A-Za-z0-9]{{{}}}", TELEGRAM_LIMIT - 1);
     proptest::string::string_regex(&prefix_regex)
@@ -74,7 +94,7 @@ proptest! {
             prop_assert!(!p.starts_with('-'));
             prop_assert!(validate_telegram_markdown(&p).is_ok());
         }
-  }
+    }
 }
 
 #[test]
